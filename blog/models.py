6472b6769e781316ff8ff49c577d7727874ed9d2
UnlimitedMatrix <unlimited.matrix.j@gmail.com>
2020-08-26 12:52:26 +0800

    adding blog app

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+# Hint: Any of the Django Model will be saved in DataBase
+
+class Post(models.Model): # defining a Django Modle "Post" by using models.Model. 
+    auther = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default = timezone.now)
+    published_date = models.DateTimeField(
+            blank = True, null = True)
+
+def pihlish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+def __str__(self):
+    return self.title
+
