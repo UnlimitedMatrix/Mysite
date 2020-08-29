af1348844e6e1817eff564dae831e8842288c5ae
UnlimitedMatrix <unlimited.matrix.j@gmail.com>
2020-08-29 12:27:29 +0800

    bug fix for veiw.py

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 from django.utils import timezone
 
@@ -9,15 +9,13 @@ class Post(models.Model): # defining a Django Modle "Post" by using models.Model
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length = 200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default = timezone.now)
-    published_date = models.DateTimeField(
-            blank = True, null = True)
+    created_date = models.DateTimeField(default = timezone.now)
+    published_date = models.DateTimeField(blank = True, null = True)
 
     def publish(self):
-    self.published_date = timezone.now()
-    self.save()
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
-    return self.title
+        return self.title
 
